3cb3db85f13c8173ca7fbed96bae190114485015
lig <lig@senthink.com>
2020-03-16 13:43:03 +0800

    添加注解模块

---
+++ annotation/src/main/java/com/demo/annotation/Main.java
@@ -0,0 +1,15 @@
+package com.demo.annotation;
+
+/**
+ * @author ligen
+ * @title: Main
+ * @projectName java2020
+ * @description:
+ * @date 2020/3/1613:41
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("111");
+    }
+
+}
