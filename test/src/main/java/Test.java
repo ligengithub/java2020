af0ba6a2804cccf71f2c90a033215f63fe68b892
lig <lig@senthink.com>
2020-06-11 18:30:51 +0800

    feat: 添加适配器模式

--- test/src/main/java/Test.java
+++ test/src/main/java/Test.java
@@ -8,6 +8,28 @@ public class Test {
 
     public static void main(String[] args) {
 
+        test1();
+    }
+
+
+    public static void test1() {
+
+        try {
+            test2();
+        } catch (Exception e) {
+            System.out.println("test1 exception");
+        }
+    }
+
+    public static void test2() {
+//        try {
+
+            System.out.println(2 / 0);
+//        } catch (Exception e) {
+//
+//            System.out.println("test2 exception");
+//
+//        }
     }
 
 }
